3bdb52539b924ca130560e45581c274806dbf471
Sachin Macwan <sachinmacwan@gmail.com>
2021-01-27 12:26:20 +1100

    added some css

--- src/components/Themes.js
+++ src/components/Themes.js
@@ -1,34 +1,34 @@
 
     const first = [
     {
-        backgroundColor: "#042424",
-        color: "#eae8ff",
-        animation:"ease 1s",
-        transition: "1s",
-        height: "100%"
-
-    },
-    {
         backgroundColor: "#3A3F55",
         color: "#B0D7FF",
         animation:"ease 1s",
-        transition: "1s",
+        transition: "2s",
         height: "100%",
-    },
-    {
-        backgroundColor: "#441A2A",
-        color: "#f2bfcc",
-        animation:"ease 1s",
-        transition: "1s", 
-        height: "100%"
     },
     {
         backgroundColor: "#274b59",
         color: "#acdff2",
         animation:"ease 1s",
-        transition: "1s", 
+        transition: "2s", 
         height: "100%"
-    }
+    },
+    {
+        backgroundColor: "#441A2A",
+        color: "#f2bfcc",
+        animation:"ease 1s",
+        transition: "2s", 
+        height: "100%"
+    },
+    {
+        backgroundColor: "#042424",
+        color: "#eae8ff",
+        animation:"ease 1s",
+        transition: "2s",
+        height: "100%"
+
+    },
 ]
 
 
